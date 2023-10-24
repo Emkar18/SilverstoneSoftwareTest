fb1fb50daa36b48c469dbe5e1ed31e91c97635d4
Emilie Karlsson <Emilie97k@gmail.com>
2023-10-24 12:24:21 +0100

    input field and button added

---
+++ SilverstoneWeatherApp/Controllers/WeatherAPIController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Web.Script.Serialization;
+using SilverstoneWeatherApp.Models;
+
+namespace SilverstoneWeatherApp.Controllers
+{
+    public class WeatherAPIController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult WeatherInfo(Location location)
+        {
+            return View(location);
+        }
+    }
+}
---
+++ SilverstoneWeatherApp/Models/Location.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SilverstoneWeatherApp.Models
+{
+    public class Location
+    {
+        public string Name { get; set; }
+    }
+}
